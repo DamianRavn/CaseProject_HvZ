b688ab19ea7cfc429cb6dfe0d7c5a064e2a9b5f2
Damian Ravn <DamianRavn@gmail.com>
2022-03-10 13:02:49 +0100

    feat: initial migration

---
+++ BackEnd/WebAPI/Migrations/20220310115631_InitialCreate.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    is_human = table.Column<bool>(type: "bit", nullable: false),
+                    is_patient_zero = table.Column<bool>(type: "bit", nullable: false),
+                    biteCode = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false),
+                    user_id = table.Column<int>(type: "int", nullable: false),
+                    userid = table.Column<int>(type: "int", nullable: true),
+                    game_id = table.Column<int>(type: "int", nullable: false),
+                    gameid = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_Players_Game_gameid",
+                        column: x => x.gameid,
+                        principalTable: "Game",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Players_User_userid",
+                        column: x => x.userid,
+                        principalTable: "User",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_gameid",
+                table: "Players",
+                column: "gameid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_userid",
+                table: "Players",
+                column: "userid");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Game");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
--- BackEnd/WebAPI/Models/HumanVZombiesDbContext.cs
+++ BackEnd/WebAPI/Models/HumanVZombiesDbContext.cs
@@ -21,43 +21,43 @@ namespace WebAPI.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Seeding data
-        //    modelBuilder.Entity<Character>().HasData(new Character() { Id = 1, Name = "Thor", Alias = "Lord of Thunder", Gender = "Male" });
-        //    modelBuilder.Entity<Character>().HasData(new Character() { Id = 2, Name = "Ironman", Alias = "Tony Stark", Gender = "Male" });
-        //    modelBuilder.Entity<Character>().HasData(new Character() { Id = 3, Name = "Black Widow", Alias = "Natasha Romanoff", Gender = "Female" });
-        //    modelBuilder.Entity<Character>().HasData(new Character() { Id = 4, Name = "Spiderman", Alias = "Peter Parker", Gender = "Male" });
-        //    modelBuilder.Entity<Character>().HasData(new Character() { Id = 5, Name = "Batman", Alias = "Bruce Wayne", Gender = "Male" });
+            //modelBuilder.Entity<Player>().HasData(new Player() { Id = 1, Name = "Thor", Alias = "Lord of Thunder", Gender = "Male" });
+            //modelBuilder.Entity<Player>().HasData(new Player() { Id = 2, Name = "Ironman", Alias = "Tony Stark", Gender = "Male" });
+            //modelBuilder.Entity<Player>().HasData(new Player() { Id = 3, Name = "Black Widow", Alias = "Natasha Romanoff", Gender = "Female" });
+            //modelBuilder.Entity<Player>().HasData(new Player() { Id = 4, Name = "Spiderman", Alias = "Peter Parker", Gender = "Male" });
+            //modelBuilder.Entity<Player>().HasData(new Player() { Id = 5, Name = "Batman", Alias = "Bruce Wayne", Gender = "Male" });
 
-        //    modelBuilder.Entity<Franchise>().HasData(new Franchise() { Id = 1, Name = "MCU", Description = "Marvel's cinematic universe." });
-        //    modelBuilder.Entity<Franchise>().HasData(new Franchise() { Id = 2, Name = "DNT", Description = "Dark Knight Trilogy" });
-        //    modelBuilder.Entity<Franchise>().HasData(new Franchise() { Id = 3, Name = "TCU", Description = "Titanic's cinematic universe." });
+            //    modelBuilder.Entity<Franchise>().HasData(new Franchise() { Id = 1, Name = "MCU", Description = "Marvel's cinematic universe." });
+            //    modelBuilder.Entity<Franchise>().HasData(new Franchise() { Id = 2, Name = "DNT", Description = "Dark Knight Trilogy" });
+            //    modelBuilder.Entity<Franchise>().HasData(new Franchise() { Id = 3, Name = "TCU", Description = "Titanic's cinematic universe." });
 
-        //    modelBuilder.Entity<Movie>().HasData(new Movie() { Id = 1, Title = "The Avengers", Genre = "Action", Year = 2012, Director = "Joss Whedon", FranchiseId = 1 });
-        //    modelBuilder.Entity<Movie>().HasData(new Movie() { Id = 2, Title = "Avengers: Endgame", Genre = "Action", Year = 2019, Director = "Anthony Russo, Joe Russo", FranchiseId = 1 });
-        //    modelBuilder.Entity<Movie>().HasData(new Movie() { Id = 3, Title = "The Dark Knight", Genre = "Action", Year = 2008, Director = "Christopher Nolan" });
-        //    modelBuilder.Entity<Movie>().HasData(new Movie() { Id = 4, Title = "Titanic", Genre = "Action", Year = 1997, Director = "James Camera" });
+            //    modelBuilder.Entity<Movie>().HasData(new Movie() { Id = 1, Title = "The Avengers", Genre = "Action", Year = 2012, Director = "Joss Whedon", FranchiseId = 1 });
+            //    modelBuilder.Entity<Movie>().HasData(new Movie() { Id = 2, Title = "Avengers: Endgame", Genre = "Action", Year = 2019, Director = "Anthony Russo, Joe Russo", FranchiseId = 1 });
+            //    modelBuilder.Entity<Movie>().HasData(new Movie() { Id = 3, Title = "The Dark Knight", Genre = "Action", Year = 2008, Director = "Christopher Nolan" });
+            //    modelBuilder.Entity<Movie>().HasData(new Movie() { Id = 4, Title = "Titanic", Genre = "Action", Year = 1997, Director = "James Camera" });
 
-        //    // Seeding m2m Character-Movie.
-        //    modelBuilder.Entity<Character>()
-        //        .HasMany(p => p.Movies)
-        //        .WithMany(m => m.Characters)
-        //        .UsingEntity<Dictionary<string, object>>(
-        //            "CharacterMovie",
-        //            r => r.HasOne<Movie>().WithMany().HasForeignKey("MovieId"),
-        //            l => l.HasOne<Character>().WithMany().HasForeignKey("CharacterId"),
-        //            je =>
-        //            {
-        //                je.HasKey("CharacterId", "MovieId");
-        //                je.HasData(
-        //                    new { CharacterId = 1, MovieId = 1 },
-        //                    new { CharacterId = 1, MovieId = 2 },
-        //                    new { CharacterId = 2, MovieId = 1 },
-        //                    new { CharacterId = 2, MovieId = 2 },
-        //                    new { CharacterId = 3, MovieId = 1 },
-        //                    new { CharacterId = 3, MovieId = 2 },
-        //                    new { CharacterId = 4, MovieId = 2 },
-        //                    new { CharacterId = 5, MovieId = 3 }
-        //                );
-        //            });
+            //    // Seeding m2m Player-Movie.
+            //    modelBuilder.Entity<Player>()
+            //        .HasMany(p => p.Movies)
+            //        .WithMany(m => m.Players)
+            //        .UsingEntity<Dictionary<string, object>>(
+            //            "PlayerMovie",
+            //            r => r.HasOne<Movie>().WithMany().HasForeignKey("MovieId"),
+            //            l => l.HasOne<Player>().WithMany().HasForeignKey("PlayerId"),
+            //            je =>
+            //            {
+            //                je.HasKey("PlayerId", "MovieId");
+            //                je.HasData(
+            //                    new { PlayerId = 1, MovieId = 1 },
+            //                    new { PlayerId = 1, MovieId = 2 },
+            //                    new { PlayerId = 2, MovieId = 1 },
+            //                    new { PlayerId = 2, MovieId = 2 },
+            //                    new { PlayerId = 3, MovieId = 1 },
+            //                    new { PlayerId = 3, MovieId = 2 },
+            //                    new { PlayerId = 4, MovieId = 2 },
+            //                    new { PlayerId = 5, MovieId = 3 }
+            //                );
+            //            });
         }
     }
 }
