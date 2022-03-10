7a658496e3f546fab1032e6fbd5bb73bd4b3d4a6
Damian Ravn <DamianRavn@gmail.com>
2022-03-10 11:34:36 +0100

    Remove ignored files

--- BackEnd/WebAPI/Controllers/PlayersController.cs
+++ BackEnd/WebAPI/Controllers/PlayersController.cs
@@ -2,149 +2,157 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Models;
 using WebAPI.Models.Domain;
+using WebAPI.Models.DTO.Player;
 
 namespace WebAPI.Controllers
 {
+    /// <summary>
+    /// The controller holds all the API endpoints for the Players table
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
     public class PlayersController : Controller
     {
         private readonly HumanVZombiesDbContext _context;
+        private readonly IMapper _mapper;
 
-        public PlayersController(HumanVZombiesDbContext context)
+        /// <summary>
+        /// Adding context and mapper with dependency injection.
+        /// </summary>
+        /// <param name="context">The proper context</param>
+        /// <param name="mapper">The automapper</param>
+        public PlayersController(HumanVZombiesDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
-        // GET: Players
-        public async Task<IActionResult> Index()
+        /// <summary>
+        /// Fetches all players in the database.
+        /// </summary>
+        /// <returns>A collection of players.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<IEnumerable<PlayerReadDTO>>> GetPlayers()
         {
-            return View(await _context.Players.ToListAsync());
+            return _mapper.Map<List<PlayerReadDTO>>(await _context.Players
+                .ToListAsync());
         }
 
-        // GET: Players/Details/5
-        public async Task<IActionResult> Details(int? id)
+        /// <summary>
+        /// Fetches a specific player in the database.
+        /// </summary>
+        /// <param name="id">The id of the player to fetch.</param>
+        /// <returns>The player, if it exists. Otherwise, the NotFound response.</returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<PlayerReadDTO>> GetPlayer(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            var player = await _context.Players.FindAsync(id);
 
-            var player = await _context.Players
-                .FirstOrDefaultAsync(m => m.id == id);
             if (player == null)
             {
                 return NotFound();
             }
 
-            return View(player);
+            return _mapper.Map<PlayerReadDTO>(player);
         }
 
-        // GET: Players/Create
-        public IActionResult Create()
+        /// <summary>
+        /// Updates a specific player with the values of the given player object.
+        /// </summary>
+        /// <param name="id">The id of the player to update.</param>
+        /// <param name="dtoPlayer">A player object containing the updated values.</param>
+        /// <returns>Nothing if the update succeeds. BadRequest if the given id doesn't match the id of the player. NotFound if the player doesn't exist.</returns>
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> PutPlayer(int id, PlayerUpdateDTO dtoPlayer)
         {
-            return View();
-        }
-
-        // POST: Players/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("id,is_human,is_patient_zero,biteCode,user_id,game_id")] Player player)
-        {
-            if (ModelState.IsValid)
+            if (id != dtoPlayer.id)
             {
-                _context.Add(player);
+                return BadRequest();
+            }
+
+            Player domainPlayer = _mapper.Map<Player>(dtoPlayer);
+
+            _context.Entry(domainPlayer).State = EntityState.Modified;
+
+            try
+            {
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
             }
-            return View(player);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PlayerExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
-        // GET: Players/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var player = await _context.Players.FindAsync(id);
-            if (player == null)
-            {
-                return NotFound();
-            }
-            return View(player);
-        }
-
-        // POST: Players/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        /// <summary>
+        /// Creates a new player based on the given player object.
+        /// </summary>
+        /// <param name="dtoPlayer">The player object.</param>
+        /// <returns>The newly created player.</returns>
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("id,is_human,is_patient_zero,biteCode,user_id,game_id")] Player player)
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Player>> PostPlayer(PlayerCreateDTO dtoPlayer)
         {
-            if (id != player.id)
-            {
-                return NotFound();
-            }
+            Player domainPlayer = _mapper.Map<Player>(dtoPlayer);
+            _context.Players.Add(domainPlayer);
+            await _context.SaveChangesAsync();
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(player);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!PlayerExists(player.id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(player);
+            return CreatedAtAction("GetPlayer", new { id = domainPlayer.id },
+                                   _mapper.Map<PlayerReadDTO>(domainPlayer));
         }
 
-        // GET: Players/Delete/5
-        public async Task<IActionResult> Delete(int? id)
+        /// <summary>
+        /// Deletes the player with the given id.
+        /// </summary>
+        /// <param name="id">The id of the player to delete.</param>
+        /// <returns>Nothing if the deletion succeeds. NotFound if no player with the given id exist.</returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeletePlayer(int id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var player = await _context.Players
-                .FirstOrDefaultAsync(m => m.id == id);
+            var player = await _context.Players.FindAsync(id);
             if (player == null)
             {
                 return NotFound();
             }
 
-            return View(player);
-        }
-
-        // POST: Players/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var player = await _context.Players.FindAsync(id);
             _context.Players.Remove(player);
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+
+            return NoContent();
         }
 
+        /// <summary>
+        /// Helper function, which checks if a player with the given id exists.
+        /// </summary>
+        /// <param name="id">The id to check for.</param>
+        /// <returns>True if a player with that id exists. False otherwise.</returns>
         private bool PlayerExists(int id)
         {
             return _context.Players.Any(e => e.id == id);
