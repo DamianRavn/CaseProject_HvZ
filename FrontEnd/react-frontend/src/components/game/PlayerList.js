8190d118f8ab1c283d4bad0eeb1a63a1b2b0f81b
FHallf <FHallf@PC1679>
2022-03-18 10:37:49 +0100

    Fixing PlayerList.

--- FrontEnd/react-frontend/src/components/game/PlayerList.js
+++ FrontEnd/react-frontend/src/components/game/PlayerList.js
@@ -1,7 +1,7 @@
 import { useGetPlayerNonAdminQuery } from "../../features/apiSlice";
 import GameItem from "./GameItem";
 
-export const PlayerList = (gameId) => {
+export const PlayerList = (props) => {
   const {
     data: players,
     isLoading,
@@ -10,17 +10,16 @@ export const PlayerList = (gameId) => {
     error,
   } = useGetPlayerNonAdminQuery();
 
-  // let gameId = 1;
   let content;
   if (isLoading) {
     content = <h1>Loading players</h1>;
   } else if (isSuccess) {
     let gamePlayerCount = 0
     players.forEach(player => {
-      if(player.game === gameId) gamePlayerCount++ 
+      if(player.game === props.gameId) gamePlayerCount++ 
     });
-    console.log("Total number of players in Game " + gameId + ": " + gamePlayerCount) 
-    content = <h1>Players in game {gameId}: {gamePlayerCount}.</h1>;
+    console.log("Total number of players in Game " + props.gameId + ": " + gamePlayerCount) 
+    content = <h1>Players in game {props.gameId}: {gamePlayerCount}.</h1>;
   } else if (isError) {
     content = <h1>{error.toString()}</h1>;
   }
