3b0888da3da5959c1ac8aeca283f6421ed786d41
Marius Samson <mariusbsamson@gmail.com>
2022-03-21 11:03:58 +0100

    FEAT: gets for Game in API include lists of player ids.

--- BackEnd/WebAPI/Profiles/GameProfile.cs
+++ BackEnd/WebAPI/Profiles/GameProfile.cs
@@ -8,23 +8,29 @@ using WebAPI.Models.DTO.Game;
 
 namespace WebAPI.Profiles
 {
+    /// <summary>
+    /// Defines the maps between Game and its DTOs.
+    /// </summary>
     public class GameProfile : Profile
     {
         public GameProfile()
         {
             CreateMap<Game, GameReadDTO>()
-                // admin in dto is admin_id
+                // Turning related players into an array of int ids.
+                .ForMember(gdto => gdto.Players, opt => opt
+                .MapFrom(g => g.Players.Select(c => c.Id).ToArray()))
+                // Include id of admin.
                 .ForMember(dto => dto.Admin, opt => opt
                 .MapFrom(a => a.AdminId))
                 .ReverseMap();
             CreateMap<GameCreateDTO, Game>()
-                //Ignore objects for DTO
+                // Ignore objects for DTO.
                 .ForMember(cdto => cdto.Admin, opt => opt.Ignore());
             CreateMap<GameUpdateDTO, Game>()
-                // admin in dto is admin_id
+                // Include id of admin.
                 .ForMember(cdto => cdto.AdminId, opt => opt
                 .MapFrom(a => a.Admin))
-                //Ignore objects for DTO
+                // Ignore objects for DTO.
                 .ForMember(cdto => cdto.Admin, opt => opt.Ignore());
         }
     }
