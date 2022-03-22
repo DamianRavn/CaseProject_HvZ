1d960d6e99700764dd32faf18b4c2d8d72297931
dragonalias <dragon1600@gmail.com>
2022-03-22 19:40:19 +0100

    Feat: Adding a killstable to keep track of score WIP

--- BackEnd/WebAPI/Controllers/KillsTablesController.cs
+++ BackEnd/WebAPI/Controllers/KillsTablesController.cs
@@ -10,109 +10,101 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using WebAPI.Models;
 using WebAPI.Models.Domain;
-using WebAPI.Models.DTO.Admin;
-using WebAPI.Models.DTO.Game;
+using WebAPI.Models.DTO.KillsTable;
 
 namespace WebAPI.Controllers
 {
     /// <summary>
     /// The controller holds all the API endpoints for the Games table.
     /// </summary>
-    [Route("api/Game")]
+    [Route("api/Kills")]
     [ApiController]
-    public class GamesController : Controller
+    public class KillsTablesController : Controller
     {
         private readonly HumanVZombiesDbContext _context;
         private readonly IMapper _mapper;
 
-        /// <summary>
-        /// Adding context and mapper with dependency injection.
-        /// </summary>
-        /// <param name="context">The proper context</param>
-        /// <param name="mapper">The automapper</param>
-        public GamesController(HumanVZombiesDbContext context, IMapper mapper/*, IHttpContextAccessor httpContextAccessor*/)
+        public KillsTablesController(HumanVZombiesDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
         /// <summary>
-        /// Fetches all games from the database.
+        /// Fetches all kills from the database.
         /// </summary>
         /// <returns>A collection of games.</returns>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<GameReadDTO>>> GetGames()
+        public async Task<ActionResult<IEnumerable<KillsReadDTO>>> GetKills()
         {
-            return _mapper.Map<List<GameReadDTO>>(await _context.Games
-                .Include(g => g.Players)
+            return _mapper.Map<List<KillsReadDTO>>(await _context.KillsTable
+                .Include(g => g.Users)
                 .ToListAsync());
         }
 
         /// <summary>
-        /// Fetches a specific game in the database.
+        /// Fetches a specific kill in the database.
         /// </summary>
-        /// <param name="id">The id of the game to fetch.</param>
-        /// <returns>The game, if it exists. Otherwise, the NotFound response.</returns>
+        /// <param name="id">The id of the kill to fetch.</param>
+        /// <returns>The kill, if it exists. Otherwise, the NotFound response.</returns>
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<GameReadDTO>> GetGame(int id)
+        public async Task<ActionResult<KillsReadDTO>> GetKill(int id)
         {
-            if (!GameExists(id))
+            if (!KillsTableExists(id))
             {
                 return NotFound();
             }
 
-            var game = await _context.Games
-                .Include(g => g.Players)
+            var kill = await _context.KillsTable
+                .Include(g => g.Users)
                 .Where(g => g.Id == id)
                 .ToListAsync();
 
-            return _mapper.Map<GameReadDTO>(game.First());
+            return _mapper.Map<KillsReadDTO>(kill.First());
         }
 
         /// <summary>
-        /// Creates a new game based on the given game object.
+        /// Creates a new kill based on the given kill object.
         /// </summary>
-        /// <param name="dtoGame">The game object.</param>
-        /// <returns>The newly created game.</returns>
-        [Authorize]
+        /// <param name="dtoGame">The kill object.</param>
+        /// <returns>The newly created kill.</returns>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<Game>> PostGame(GameCreateDTO dtoGame)
+        public async Task<ActionResult<KillsTable>> PostKill(KillsCreateDTO dtoKill)
         {
-            Game domainGame = _mapper.Map<Game>(dtoGame);
+            KillsTable domainKill = _mapper.Map<KillsTable>(dtoKill);
 
-            _context.Games.Add(domainGame);
+            _context.KillsTable.Add(domainKill);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetGame", new { id = domainGame.Id },
-                                   _mapper.Map<GameReadDTO>(domainGame));
+            return CreatedAtAction("GetGame", new { id = domainKill.Id },
+                                   _mapper.Map<KillsReadDTO>(domainKill));
         }
 
         /// <summary>
-        /// Updates a specific game with the values of the given game object.
+        /// Updates a specific kill with the values of the given kill object.
         /// </summary>
-        /// <param name="id">The id of the game to update.</param>
-        /// <param name="dtoGame">A game object containing the updated values.</param>
-        /// <returns>Nothing if the update succeeds. BadRequest if the given id doesn't match the id of a game. NotFound if the game doesn't exist.</returns>
-        [Authorize]
+        /// <param name="id">The id of the kill to update.</param>
+        /// <param name="dtoKill">A kill object containing the updated values.</param>
+        /// <returns>Nothing if the update succeeds. BadRequest if the given id doesn't match the id of a kill. NotFound if the kill doesn't exist.</returns>
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<IActionResult> PutGame(int id, GameUpdateDTO dtoGame)
+        public async Task<IActionResult> PutKill(int id, KillsUpdateDTO dtoKill)
         {
-            if (id != dtoGame.Id)
+            if (id != dtoKill.Id)
             {
                 return BadRequest();
             }
 
-            Game domainGame = _mapper.Map<Game>(dtoGame);
+            KillsTable domainGame = _mapper.Map<KillsTable>(dtoKill);
 
             _context.Entry(domainGame).State = EntityState.Modified;
 
@@ -122,7 +114,7 @@ namespace WebAPI.Controllers
             }
             catch (DbUpdateConcurrencyException)
             {
-                if (!GameExists(id))
+                if (!KillsTableExists(id))
                 {
                     return NotFound();
                 }
@@ -136,13 +128,13 @@ namespace WebAPI.Controllers
         }
 
         /// <summary>
-        /// Helper function, which checks if a game with the given id exists.
+        /// Helper function, which checks if a kill with the given id exists.
         /// </summary>
         /// <param name="id">The id to check for.</param>
-        /// <returns>True if a game with that id exists. False otherwise.</returns>
-        private bool GameExists(int id)
+        /// <returns>True if a kill with that id exists. False otherwise.</returns>
+        private bool KillsTableExists(int id)
         {
-            return _context.Games.Any(e => e.Id == id);
+            return _context.KillsTable.Any(e => e.Id == id);
         }
     }
 }
---
+++ BackEnd/WebAPI/Profiles/KillsTableProfile.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models.Domain;
+using WebAPI.Models.DTO.KillsTable;
+
+namespace WebAPI.Profiles
+{
+    /// <summary>
+    /// Defines the maps between Game and its DTOs.
+    /// </summary>
+    public class KillsTableProfile : Profile
+    {
+        public KillsTableProfile()
+        {
+            CreateMap<KillsTable, KillsReadDTO>()
+                // Turning related users into an array of int ids.
+                .ForMember(gdto => gdto.Users, opt => opt
+                .MapFrom(g => g.Users.Select(c => c.Id).ToArray()))
+                // Include id of user.
+                .ForMember(dto => dto.User, opt => opt
+                .MapFrom(a => a.UserId))
+                .ReverseMap();
+            CreateMap<KillsCreateDTO, KillsTable>()
+                //Ignore objects for DTO
+                .ForMember(cdto => cdto.User, opt => opt.Ignore())
+                .ForMember(cdto => cdto.Users, opt => opt.Ignore());
+            CreateMap<KillsUpdateDTO, KillsTable>();
+        }
+    }
+}
